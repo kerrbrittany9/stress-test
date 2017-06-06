7a4337c00c3e1bca96e2a427c2d37045f07ce80d
Brittany Kerr and Larry Taylor <brittany@>
2017-06-06 14:32:26 -0700

    finish code, results work

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,27 @@ $(document).ready(function(){
     event.preventDefault();
 
 
-    var warningSigns = 0  $("input:checkbox[name=warningSigns]:checked").each(function(){
-    var stressors = parseInt($(this).val())
-    stressors += warningSigns
+    var warningSigns = 0;  $("input:checkbox[name=warningSigns]:checked").each(function(){
+      var stressors = parseInt($(this).val());
+      warningSigns += stressors;
 
     });
-    var healthSigns = 0 $("input:checkbox[name=healthSigns]:checked").each(function(){
+    var healthSigns = 0; $("input:checkbox[name=healthSigns]:checked").each(function(){
+      var stressors = parseInt($(this).val());
+      healthSigns += stressors;
 
     });
-    var copingMethods = 0 $("input:checkbox[name=copingMethods]:checked").each(function(){
-
+    var copingMethods = 0; $("input:checkbox[name=copingMethods]:checked").each(function(){
+      var stressors = parseInt($(this).val());
+      copingMethods += stressors;
     });
+    $(".result").hide();
+    if (warningSigns >= 3 && healthSigns >= 4 && copingMethods <= 2){
+      $("#dieSoon").show();
+    } else if (warningSigns >= 3 && healthSigns >= 3 && copingMethods >= 3){
+      $("#doBetter").show();
+    } else {
       $("#goodJob").show();
-        $("#doBetter").show();
-            $("#dieSoon").show();
+    }
   });
 });
